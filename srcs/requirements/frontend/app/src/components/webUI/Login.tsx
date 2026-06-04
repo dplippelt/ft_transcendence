6e182bb68908e7a67d0d6b9c1a1116af5275716f
dlippelt <dlippelt@student.codam.nl>
2026-06-04 11:12:50 +0200

    WebUI - ADD: add (mock) login and sign-up checks and show error message to the user when credentials are invalid

--- srcs/requirements/frontend/app/src/components/webUI/Login.tsx
+++ srcs/requirements/frontend/app/src/components/webUI/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function PageTitle()
@@ -7,17 +8,32 @@ function PageTitle()
 
 function LoginQuery()
 {
+	const [username, setUsername] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [error, setError] = useState<boolean>(false);
 	const navigate = useNavigate();
+
+	function checkLogin()
+	{
+		//if invalid username + password combo setError(true)
+		//else navigate("/main-menu")
+
+		// Mock login check:
+		if ( username !== password )
+			return setError(true);
+		navigate("/main-menu");
+	}
 
 	return (
 		<div className="loginSignupQuery">
+			{ error && <div className="incorrectCreds">Incorrect username or password!</div> }
 			<div className="text">Username:</div>
-			<input className="textInput" type="text" placeholder="Enter username"/>
+			<input className="textInput" autoComplete="off" type="text" placeholder="Enter username" onChange={ (e) => setUsername(e.target.value) }/>
 			<div className="text">Password:</div>
-			<input className="textInput" type="text" placeholder="Enter password"/>
+			<input className="textInput" autoComplete="off" type="password" placeholder="Enter password" onChange={ (e) => setPassword(e.target.value) }/>
 			<div className="loginSignupButtons">
 				<button className="buttonV1" onClick={ () => navigate("/signup") }>Sign-up</button>
-				<button className="buttonV1" onClick={ () => navigate("/main-menu") }>Login</button>
+				<button className="buttonV1" onClick={ checkLogin }>Login</button>
 			</div>
 			<button className="buttonV1">Continue with Google</button>
 		</div>
