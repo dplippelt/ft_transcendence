d18e59cb28fe056e076741cdd2e2350351320441
dlippelt <dlippelt@student.codam.nl>
2026-06-10 09:36:58 +0200

    Settings page FT-35 - ADD: add Apply button. Settings are no longer auto saved when changing, hitting Apply is required (for backend integration Apply will then also save to the database). - IMPROVE: clicking the label for slider or dropdown elements will now also focus the number input and dropdown element respectively

--- srcs/requirements/frontend/app/src/components/webUI/Settings.tsx
+++ srcs/requirements/frontend/app/src/components/webUI/Settings.tsx
@@ -1,30 +1,50 @@
 import { useNavigate } from "react-router-dom";
 import { useSettings } from "../../contexts/SettingsContext";
 import styles from "./Settings.module.scss";
+import { useRef, useState } from "react";
 
 type CheckboxProps =
 {
+	ref: React.RefObject<HTMLInputElement | null>,
 	label: string,
 	id: string,
 	setting: boolean,
-	onChange: ( e: React.ChangeEvent<HTMLInputElement, HTMLInputElement> ) => void,
 }
 
 type SliderProps =
 {
+	ref: React.RefObject<HTMLInputElement | null>,
 	label: string,
+	id: string,
 	min: number,
 	max: number,
 	setting: number,
-	onChange: ( e: React.ChangeEvent<HTMLInputElement, HTMLInputElement> ) => void,
 }
 
 type DropdownProps =
 {
+	ref: React.RefObject<HTMLSelectElement | null>,
 	label: string,
+	id: string,
 	options: { value: string, label:string }[],
 	setting: string,
-	onChange: ( e: React.ChangeEvent<HTMLSelectElement, HTMLSelectElement> ) => void,
+}
+
+type SettingsWindowProps =
+{
+	settingRefs: SettingRefs,
+}
+
+type ButtonsProps =
+{
+	settingRefs: SettingRefs,
+}
+
+type SettingRefs =
+{
+	dummyBoolean: React.RefObject<HTMLInputElement | null>,
+	dummySlider: React.RefObject<HTMLInputElement | null>,
+	dummyDropdown: React.RefObject<HTMLSelectElement | null>,
 }
 
 function PageTitle()
@@ -32,52 +52,57 @@ function PageTitle()
 	return <div className="menuTitle">Settings</div>
 }
 
-function Checkbok( { label, id, setting, onChange } : CheckboxProps )
+function Checkbok( { ref, label, id, setting } : CheckboxProps )
 {
 	return (
 		<div className={styles.checkbox}>
 			<label htmlFor={id}>{label}</label>
 			<input
+				ref={ref}
 				id={id}
 				type="checkbox"
-				checked={setting}
-				onChange={onChange}/>
+				defaultChecked={setting}/>
 		</div>
 	);
 }
 
-function Slider( { label, min, max, setting, onChange } : SliderProps )
+function Slider( { ref, label, id, min, max, setting } : SliderProps )
 {
+	const [value, setValue] = useState<number>(setting);
+
 	return (
 		<div className={styles.slider}>
-			<label>{label}</label>
+			<label htmlFor={id}>{label}</label>
 			<div className={styles.track}>
 				<input
+					ref={ref}
 					type="range"
 					min={min}
 					max={max}
-					value={setting}
-					onChange={onChange}/>
+					value={value}
+					onChange={ (e) => setValue(Number(e.target.value))}/>
 				<input
+					id={id}
 					style={{marginLeft: "1rem"}}
 					type="number"
 					min={min}
 					max={max}
-					value={setting}
-					onChange={onChange}/>
+					value={value}
+					onChange={ (e) => setValue(Number(e.target.value))}/>
 			</div>
 		</div>
 	);
 }
 
-function Dropdown( { label, options, setting, onChange } : DropdownProps )
+function Dropdown( { ref, label, id, options, setting } : DropdownProps )
 {
 	return (
 		<div className={styles.dropdown}>
-			<label>{label}</label>
+			<label htmlFor={id}>{label}</label>
 			<select
-				value={setting}
-				onChange={onChange}
+				ref={ref}
+				id={id}
+				defaultValue={setting}
 			>
 				{ options.map((opt) =>
 					<option key={opt.value} value={opt.value}>{opt.label}</option> ) }
@@ -86,41 +111,62 @@ function Dropdown( { label, options, setting, onChange } : DropdownProps )
 	);
 }
 
-function SettingsWindow()
+function SettingsWindow( { settingRefs } : SettingsWindowProps )
 {
 	const settings = useSettings();
 
 	return (
 		<div className={styles.settingsWindow}>
-			<Checkbok label="Dummy boolean" id="dummyBoolean" setting={settings.dummyBoolean} onChange={ (e) => settings.setDummyBoolean(e.target.checked) } />
-			<Slider label="Dummy slider" min={0} max={100} setting={settings.dummySlider} onChange={ (e) => settings.setDummySlider(Number(e.target.value)) } />
-			<Dropdown label="Dummy dropdown" options={[ { value: "default", label: "Default setting" }, { value: "alt", label: "Alternative setting" } ]} setting={settings.dummyDropdown} onChange={ (e) => settings.setDummyDropdown(e.target.value) } />
+			<Checkbok ref={settingRefs.dummyBoolean} label="Dummy boolean" id="dummyBoolean" setting={settings.dummyBoolean} />
+			<Slider ref={settingRefs.dummySlider} label="Dummy slider" id="dummySlider" min={0} max={100} setting={settings.dummySlider} />
+			<Dropdown ref={settingRefs.dummyDropdown} label="Dummy dropdown" id="dummyDropdown" options={[ { value: "default", label: "Default setting" }, { value: "alt", label: "Alternative setting" } ]} setting={settings.dummyDropdown} />
 		</div>
 	);
 }
 
-function Buttons()
+function Buttons( { settingRefs } : ButtonsProps )
 {
 	const navigate = useNavigate();
 	const settings = useSettings();
 
+	function applySettings()
+	{
+		settings.setDummyBoolean(settingRefs.dummyBoolean.current!.checked);
+		settings.setDummySlider(Number(settingRefs.dummySlider.current!.value));
+		settings.setDummyDropdown(settingRefs.dummyDropdown.current!.value);
+
+		// add saving settings to database here
+	}
+
 	return (
 		<div className="bottomButtons">
-			<button className="buttonV2 backButton" onClick={ () => navigate(-1) }>Back</button>
-			<button className="buttonV2" onClick={settings.resetSettings}>Reset Defaults</button>
+			<button className="buttonV2 mobileBottom" onClick={ () => navigate(-1) }>Back</button>
+			<button className="buttonV2 mobileTop" onClick={settings.resetSettings}>Reset Defaults</button>
+			<button className="buttonV2" onClick={applySettings}>Apply</button>
 		</div>
 	);
 }
 
 export default function Settings()
 {
+	const dummyBooleanRef = useRef<HTMLInputElement | null>(null);
+	const dummySliderRef = useRef<HTMLInputElement | null>(null);
+	const dummyDropdownRef = useRef<HTMLSelectElement | null>(null);
+
+	const settingRefs: SettingRefs =
+	{
+		dummyBoolean: dummyBooleanRef,
+		dummySlider: dummySliderRef,
+		dummyDropdown: dummyDropdownRef,
+	}
+
 	return (
 		<>
 			<div className="background" />
 			<div className="page">
 				<PageTitle/>
-				<SettingsWindow/>
-				<Buttons/>
+				<SettingsWindow settingRefs={settingRefs} />
+				<Buttons settingRefs={settingRefs}/>
 			</div>
 		</>
 	);
