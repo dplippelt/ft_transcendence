720e60ba6ccdbc37e8382def01ad9b831c5fffb9
Takato Mitsuya <takatomitsuya@TakatonoMacBook-Air.local>
2026-06-15 17:45:02 +0200

    added Combat Scene to game config

--- services/frontend/app/src/game/main.ts
+++ services/frontend/app/src/game/main.ts
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import BootScene from "./scenes/BootScene";
 import PreloadScene from "./scenes/PreloadScene";
 import GameScene from "./scenes/GameScene";
+import Combat from "./scenes/combat/Combat";
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -16,7 +17,7 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
   loader: {
     path: "./assets",
   },
-  scene: [BootScene, PreloadScene, GameScene],
+  scene: [BootScene, PreloadScene, GameScene, Combat],
   physics: {
     default: "arcade",
     arcade: {
