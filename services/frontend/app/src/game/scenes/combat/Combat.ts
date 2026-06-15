c84ff8ea0f30861fcabb05e8954bdffe2d8b9fcb
Takato Mitsuya <takatomitsuya@TakatonoMacBook-Air.local>
2026-06-15 17:42:45 +0200

    first setup Combat Scene class with sample card hand setup

--- services/frontend/app/src/game/scenes/combat/Combat.ts
+++ services/frontend/app/src/game/scenes/combat/Combat.ts
@@ -1,20 +1,63 @@
 import Phaser from "phaser";
+import { EventBus } from "../../EventBus";
+import CardHand from "./CardHand";
+import NumberCard from "./NumberCard";
+import OperatorCard, { Operator } from "./OperatorCard";
 
 export default class Combat extends Phaser.Scene {
+	private cardHand!: CardHand;
+
 	constructor() {
 		super("combat");
 	}
 
 	preload() {
-
+		// load images for the combat scene
+		// needs to know which enemy the player is going to fight
+		// needs to know the status such as health point or the items (?) it has
+		// temporarily set the background color for ths combat scene, needs to be replace by an image
+		const backgroundColor = new Phaser.Display.Color(200, 200, 200);
+		this.cameras.main.setBackgroundColor(backgroundColor.color);
 	}
 
 	create() {
-		
 
+		this.cardHand = new CardHand(this);
+
+        // test for creation and alignment of hand of cards
+        this.sampleInitCardHand();
+
+		EventBus.emit('current-scene-ready', this);
+
+        // TODOs
+		// const selectedCard = initSelectedCard;
+		// const timer = initTimer();
+		// const player = new PlayerCombat(this, 100, 100);
+		// const enemy = new EnemyCombat(this, 900, 100);
+
+        this.input.once('pointerdown', () => {
+            this.scene.stop().wake('game');
+        })
 	}
 
 	update() {
 
+
 	}
+
+    sampleInitCardHand() {
+
+        for (let i = 1; i <= 5; ++i) {
+            this.cardHand.addCard(new NumberCard(this, i));
+        }
+
+        this.cardHand.addCard(new OperatorCard(this, Operator.Plus));
+        this.cardHand.addCard(new OperatorCard(this, Operator.Minus));
+        this.cardHand.addCard(new OperatorCard(this, Operator.Multiply));
+
+        this.cardHand.suffle();
+        this.cardHand.align();
+
+    }
+
 }
