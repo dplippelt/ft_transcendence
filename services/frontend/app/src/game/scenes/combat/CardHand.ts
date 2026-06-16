1c1176b60141dcf16d4a5a193cc7532caa0911eb
Takato Mitsuya <tmitsuya@f1r1s8.codam.nl>
2026-06-16 15:05:23 +0200

    change the type of cards in CardHand from an array to GameObjects.Container

--- services/frontend/app/src/game/scenes/combat/CardHand.ts
+++ services/frontend/app/src/game/scenes/combat/CardHand.ts
@@ -3,34 +3,30 @@ import type CardBase from "./CardBase";
 
 export default class CardHand {
 
-    private readonly cards: CardBase[] = [];
-    // private readonly scene!: Scene;
-    private readonly displayList!: GameObjects.DisplayList;
+    private readonly cards!: GameObjects.Container;
 
     constructor(scene: Scene) {
 
-        // this.scene = scene;
-        this.displayList = scene.children;
+		this.cards = scene.add.container(100, 400);
 
     };
 
     addCard(card: CardBase) {
 
-        this.cards.push(card);
+        this.cards.add(card);
 
     };
 
     suffle() {
-        Actions.Shuffle(this.cards);
-    };
+
+		this.cards.shuffle();
+
+	};
 
     align() {
 
         const line = new Geom.Line(100, 400, 800, 400);
-        Actions.PlaceOnLine(this.cards, line);
-        this.cards.forEach((card: CardBase) => {
-            this.displayList.bringToTop(card);
-        });
+        Actions.PlaceOnLine(this.cards.getAll(), line);
 
         // Actions.AlignTo(this.cardHand.getChildren(), Display.Align.RIGHT_CENTER, 10, 30);
         // const ellipse = new Geom.Ellipse(400, 300, 400, 200);
