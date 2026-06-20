f9846ac888ac1afbf68ba8bb36f412093362ca82
Takato Mitsuya <takatomitsuya@TakatonoMacBook-Air.local>
2026-06-20 18:15:09 +0200

    Added the update() function for rendering the card hand and selected cards and refactored the configurations as well

--- services/frontend/app/src/game/gameobjects/cards/CardHand.ts
+++ services/frontend/app/src/game/gameobjects/cards/CardHand.ts
@@ -1,15 +1,78 @@
-import { Scene, Actions, GameObjects, Geom } from "phaser";
-import type CardBase from "./CardBase";
+import { Scenes, Scene, Actions, GameObjects, Geom, Game } from "phaser";
+import CardBase from "./CardBase";
+
+interface CardHandConfig {
+    firstCardCenterX: number;
+    firstCardCenterY: number;
+    handStartX: number;
+    handStartY: number;
+    handEndX: number;
+    handEndY: number;
+    focusDiff: number;
+    selectionCardStartX: number,
+    selectionCardStartY: number,
+    selectionCardEndX: number,
+    selectionCardEndY: number,
+}
+
+const cardHandConfig: CardHandConfig = {
+    firstCardCenterX: 0,
+    firstCardCenterY: 0,
+    handStartX: 100,
+    handStartY: 400,
+    handEndX: 800,
+    handEndY: 400,
+    focusDiff: 30,
+    selectionCardStartX: 800,
+    selectionCardStartY: 100,
+    selectionCardEndX: 900,
+    selectionCardEndY: 100,
+}
 
 export default class CardHand {
 
+    private readonly cardHandConfig!: CardHandConfig;
     private readonly cards!: GameObjects.Container;
+    private handLine!: Geom.Line;
+    private selectionLine!: Geom.Line;
 
     constructor(scene: Scene) {
 
-		this.cards = scene.add.container(100, 400);
+        this.cardHandConfig = cardHandConfig;
+
+        this.cards = scene.add.container(
+            this.cardHandConfig.firstCardCenterX, 
+            this.cardHandConfig.firstCardCenterY,
+        );
+
+        this.cards.addToUpdateList();
+        scene.events.on(Scenes.Events.UPDATE, this.update, this);
+        this.cards.once(GameObjects.Events.DESTROY, () => {
+            scene.events.off(Scenes.Events.UPDATE, this.update, this);
+        }, this);
+
+        scene.input.setTopOnly(true);
+
+        this.handLine = new Geom.Line(
+            this.cardHandConfig.handStartX,
+            this.cardHandConfig.handStartY,
+            this.cardHandConfig.handEndX,
+            this.cardHandConfig.handEndY,
+        );
+
+        this.selectionLine = new Geom.Line(
+            this.cardHandConfig.selectionCardStartX,
+            this.cardHandConfig.selectionCardStartY,
+            this.cardHandConfig.selectionCardEndX,
+            this.cardHandConfig.selectionCardEndY,
+        )
 
     };
+
+    update() {
+        this.align();
+    }
+
 
     addCard(card: CardBase) {
 
@@ -25,8 +88,9 @@ export default class CardHand {
 
     align() {
 
-        const line = new Geom.Line(100, 400, 800, 400);
-        Actions.PlaceOnLine(this.cards.getAll(), line);
+        // Actions.PlaceOnLine(this.cards.getAll(), line);
+        Actions.PlaceOnLine(this.cards.getAll('isSelected', false), this.handLine);
+        Actions.PlaceOnLine(this.cards.getAll('isSelected', true), this.selectionLine);
 
         // Actions.AlignTo(this.cardHand.getChildren(), Display.Align.RIGHT_CENTER, 10, 30);
         // const ellipse = new Geom.Ellipse(400, 300, 400, 200);
