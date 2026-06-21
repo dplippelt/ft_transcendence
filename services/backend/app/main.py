aeb3f9ed9b7ab667bc33d9336d2a95f82b4168c5
bewong <bewong@student.codam.nl>
2026-06-21 16:19:20 +0200

    added auth and users router

--- services/backend/app/main.py
+++ services/backend/app/main.py
@@ -1,32 +1,22 @@
 from fastapi import FastAPI
-from typing import Annotated
 
-from app.api.v1 import users, dungeons, cards, puzzles, scores, leaderboard
-
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-from api import auth, users
-from models.user import User
-
-from db.database import Base, get_db, engine
-from models.dungeon import Dungeon
+from app.db.database import Base, engine
+from app.models.user import User
+from app.api.v1 import auth, users, dungeons, cards, puzzles, scores, leaderboard
 
 app = FastAPI(title="ft_transcendence API", version="1.0.0")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.on_event("startup")
 def on_startup():
   Base.metadata.create_all(bind=engine)
 
-app.include_router(users.router,       prefix="/users",       tags=["users"])
-app.include_router(dungeons.router,    prefix="/dungeons",    tags=["dungeons"])
-app.include_router(cards.router,       prefix="/cards",       tags=["cards"])
-app.include_router(puzzles.router,     prefix="/puzzles",     tags=["puzzles"])
-app.include_router(scores.router,      prefix="/scores",      tags=["scores"])
-app.include_router(leaderboard.router, prefix="/leaderboard", tags=["leaderboard"])
-app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
-app.include_router(users.router, prefix="/api/users", tags=["users"])
+app.include_router(auth.router,        prefix="/auth",         tags=["auth"])
+app.include_router(users.router,       prefix="/users",        tags=["users"])
+app.include_router(dungeons.router,    prefix="/dungeons",     tags=["dungeons"])
+app.include_router(cards.router,       prefix="/cards",        tags=["cards"])
+app.include_router(puzzles.router,     prefix="/puzzles",      tags=["puzzles"])
+app.include_router(scores.router,      prefix="/scores",       tags=["scores"])
+app.include_router(leaderboard.router, prefix="/leaderboard",  tags=["leaderboard"])
 
 @app.get("/")
 def root():
