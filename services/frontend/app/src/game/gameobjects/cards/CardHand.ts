868d35db7fcd7a86ce097c316d288407ac76ad5b
Takato Mitsuya <takatomitsuya@TakatonoMacBook-Air.local>
2026-06-21 17:41:08 +0200

    Moved card visual logic to CardHand by having it listen to card input events

--- services/frontend/app/src/game/gameobjects/cards/CardHand.ts
+++ services/frontend/app/src/game/gameobjects/cards/CardHand.ts
@@ -1,5 +1,7 @@
 import { Scenes, Scene, Actions, GameObjects, Geom, Game } from "phaser";
-import CardBase from "./CardBase";
+import CardBase, { CardEvents } from "./CardBase";
+import NumberCard from "./NumberCard";
+import OperatorCard from "./OperatorCard";
 
 interface CardHandConfig {
     firstCardCenterX: number;
@@ -8,11 +10,18 @@ interface CardHandConfig {
     handStartY: number;
     handEndX: number;
     handEndY: number;
-    focusDiff: number;
     selectionCardStartX: number,
     selectionCardStartY: number,
     selectionCardEndX: number,
     selectionCardEndY: number,
+    selectionLimit: {
+        number: number,
+        operator: number,
+    }
+    focus: {
+        diffX: number,
+        diffY: number,
+    }
 }
 
 const cardHandConfig: CardHandConfig = {
@@ -22,11 +31,18 @@ const cardHandConfig: CardHandConfig = {
     handStartY: 400,
     handEndX: 800,
     handEndY: 400,
-    focusDiff: 30,
     selectionCardStartX: 800,
     selectionCardStartY: 100,
     selectionCardEndX: 900,
     selectionCardEndY: 100,
+    selectionLimit: {
+        number: 2,
+        operator: 1,
+    },
+    focus: {
+        diffX: 0,
+        diffY: 30,
+    }
 }
 
 export default class CardHand {
@@ -76,6 +92,10 @@ export default class CardHand {
 
     addCard(card: CardBase) {
 
+        card.on(CardEvents.FOCUSON, this.focusOn, this);
+        card.on(CardEvents.FOCUSOFF, this.focusOff, this);
+        card.on(CardEvents.SELECTION, this.select, this);
+
         this.cards.add(card);
 
     };
@@ -88,13 +108,92 @@ export default class CardHand {
 
     align() {
 
-        // Actions.PlaceOnLine(this.cards.getAll(), line);
         Actions.PlaceOnLine(this.cards.getAll('isSelected', false), this.handLine);
+
         Actions.PlaceOnLine(this.cards.getAll('isSelected', true), this.selectionLine);
+
+        const focusedCard = this.cards.getFirst("isFocused", true) as CardBase;
+        if (focusedCard?.input?.hitArea instanceof Geom.Rectangle) {
+
+            const focus = this.cardHandConfig.focus;
+            focusedCard.x -= focus.diffX;
+            focusedCard.y -= focus.diffY;
+
+        }
 
         // Actions.AlignTo(this.cardHand.getChildren(), Display.Align.RIGHT_CENTER, 10, 30);
         // const ellipse = new Geom.Ellipse(400, 300, 400, 200);
         // Actions.PlaceOnEllipse(this.cardHand.getChildren(), ellipse, Math.PI, 0);
 
     };
+
+    focusOn(card: CardBase) {
+
+        const focus = this.cardHandConfig.focus;
+        if (card.input?.hitArea instanceof Geom.Rectangle) {
+            card.input.hitArea.right += focus.diffX;
+            card.input.hitArea.bottom += focus.diffY;            
+        }
+
+    }
+
+    focusOff(card: CardBase) {
+
+        const focus = this.cardHandConfig.focus;
+        if (card.input?.hitArea instanceof Geom.Rectangle) {
+            card.input.hitArea.right -= focus.diffX;
+            card.input.hitArea.bottom -= focus.diffY;            
+        }
+
+    }
+
+    select(card: CardBase) {
+
+        const isAlreadySelected = card.getIsSelected();
+
+        if (isAlreadySelected) {
+            
+            card.setIsSelected(false);
+            return ;
+
+        }
+
+        const limits = this.cardHandConfig.selectionLimit;
+        const isAlreadyFull = (children: CardBase[], limit: number): boolean => {
+
+            let count = 0;
+            for (let child of children) {
+                if (child.getIsSelected())
+                    ++count;
+            }
+
+            return count === limit;
+
+        }
+
+        if (card instanceof NumberCard) {
+
+            const numberCards = this.cards.getAll().filter(
+                (child): child is NumberCard => child instanceof NumberCard 
+            );
+
+            if (isAlreadyFull(numberCards, limits.number))
+                return ;
+
+        } else if (card instanceof OperatorCard) {
+
+            const operatorCards = this.cards.getAll().filter(
+                (child): child is OperatorCard => child instanceof OperatorCard
+            );
+
+            if (isAlreadyFull(operatorCards, limits.operator))
+                return ;
+
+        } else {
+            return ;
+        }
+
+        card.setIsSelected(true);
+
+    }
 }
