a0bf9437d0443c0680e8800755e0bc4681b92d17
bewong <bewong@student.codam.nl>
2026-06-23 19:48:10 +0200

    add settings.py to centralized all configurable values

--- services/backend/app/core/security.py
+++ services/backend/app/core/security.py
@@ -1,37 +1,51 @@
 from datetime import datetime, timedelta, timezone
 
-import os
 import jwt
-from pwdlib import PasswordHash
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+from pwdlib import PasswordHash
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from app.core.settings import get_settings
 
+
+settings = get_settings()
 password_hash = PasswordHash.recommended()
 
-DUMMTY_PASSWORD_HASH = password_hash.hash("")
+DUMMY_PASSWORD_HASH = password_hash.hash("")
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-  return password_hash.verify(plain_password, hashed_password)
+    return password_hash.verify(plain_password, hashed_password)
+
 
 def get_password_hash(password: str) -> str:
-  return password_hash.hash(password)
+    return password_hash.hash(password)
 
-def create_access_token(data: dict,  expires_delta: timedelta | None = None) -> str:
-  to_encode = data.copy()
-  if expires_delta:
-   expire = datetime.now(timezone.utc) + expires_delta
-  else:
-   expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-  to_encode.update({"exp": expire})
-  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-  return encoded_jwt
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.access_token_expire_minutes
+        )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        settings.secret_key,
+        algorithm=settings.algorithm,
+    )
+
 
 def decode_access_token(token: str) -> dict | None:
-  try:
-   payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-   return payload
-  except (ExpiredSignatureError, InvalidTokenError):
-   return None
+    try:
+        return jwt.decode(
+            token,
+            settings.secret_key,
+            algorithms=[settings.algorithm],
+        )
+    except (ExpiredSignatureError, InvalidTokenError):
+        return None
