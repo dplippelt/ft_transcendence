e6259737624d5cdb6e15ad136db4fba1bcfdca05
Takato Mitsuya <142211753+takato7@users.noreply.github.com>
2026-07-07 14:43:43 +0200

    Format code with Prettier and convert some variables from let to const

--- services/frontend/app/src/game/gameobjects/cards/CardSelection.ts
+++ services/frontend/app/src/game/gameobjects/cards/CardSelection.ts
@@ -5,161 +5,128 @@ import OperatorCard from "./OperatorCard";
 import CardSlot, { cardSlotStyleConfig } from "./CardSlot";
 import { type CardStyleConfig } from "./CardStyle";
 
-
 interface SelectedCardAlignConfig {
-    firstSlotCenter: {
-        x: number,
-        y: number,
-    },
-    gridOptions: Phaser.Types.Actions.GridAlignConfig,
+  firstSlotCenter: {
+    x: number;
+    y: number;
+  };
+  gridOptions: Phaser.Types.Actions.GridAlignConfig;
 }
 
 const selectedCardAlignConfig: SelectedCardAlignConfig = {
-    firstSlotCenter: {
-        x: 0,
-        y: 0,
-    },
-    gridOptions: {
-        width: -1,
-        cellWidth: 100,
-        x: 100,
-        y: 100,
-    }
-}
+  firstSlotCenter: {
+    x: 0,
+    y: 0,
+  },
+  gridOptions: {
+    width: -1,
+    cellWidth: 100,
+    x: 100,
+    y: 100,
+  },
+};
 
 export default class CardSelection {
+  // define and set slots for selected cards
+  // define the maximum number of number cards to set (the number of operator is minus 1)
+  // add number and operator cards into slots when there is an available slot
+  // unset cards from the slots when they are selected again but remain the positions of the rest cards
 
-    // define and set slots for selected cards 
-    // define the maximum number of number cards to set (the number of operator is minus 1)
-    // add number and operator cards into slots when there is an available slot
-    // unset cards from the slots when they are selected again but remain the positions of the rest cards
+  private readonly cardSlotStyleConfig!: CardStyleConfig;
+  private readonly selectedCardAlignConfig!: SelectedCardAlignConfig;
+  private readonly slots!: Phaser.GameObjects.Container;
+  private numSlots!: number;
 
-    private readonly cardSlotStyleConfig!: CardStyleConfig;
-    private readonly selectedCardAlignConfig!: SelectedCardAlignConfig;
-    private readonly slots!: Phaser.GameObjects.Container;
-    private numSlots!: number;
+  constructor(scene: Scene, numSlots: number) {
+    this.cardSlotStyleConfig = cardSlotStyleConfig;
+    this.selectedCardAlignConfig = selectedCardAlignConfig;
 
-    constructor(scene: Scene, numSlots: number) {
-        
-        this.cardSlotStyleConfig = cardSlotStyleConfig;
-        this.selectedCardAlignConfig = selectedCardAlignConfig;
+    this.slots = scene.add.container(
+      this.selectedCardAlignConfig.firstSlotCenter.x,
+      this.selectedCardAlignConfig.firstSlotCenter.y,
+    );
 
-        this.slots = scene.add.container(
-            this.selectedCardAlignConfig.firstSlotCenter.x,
-            this.selectedCardAlignConfig.firstSlotCenter.y,
-        );
+    this.numSlots = numSlots;
 
-        this.numSlots = numSlots;
+    for (let i = 0; i < this.numSlots; ++i) {
+      this.slots.add(new CardSlot(scene, this.cardSlotStyleConfig));
+    }
+  }
 
-        for (let i = 0; i < this.numSlots; ++i) {
-            this.slots.add(
-                new CardSlot(scene, this.cardSlotStyleConfig)
-            );
-        }
-    };
+  setCardToSlot(card: CardBase): boolean {
+    const slots = this.slots.getAll() as CardSlot[];
 
-    setCardToSlot(card: CardBase): boolean {
+    for (let i = 0; i < this.numSlots; ++i) {
+      const slot = slots[i];
 
-        const slots = this.slots.getAll() as CardSlot[];
+      if (slot.isCardSet()) continue;
 
-        for (let i = 0; i < this.numSlots; ++i) {
+      slot.setCard(card);
 
-            let slot = slots[i];
-
-            if (slot.isCardSet())
-                continue ;
-
-            slot.setCard(card);
-
-            return true;
-        }
-
-        return false;
-
-    };
-
-    unsetCardFromSlot(card: CardBase): boolean {
-
-        const slots = this.slots.getAll() as CardSlot[];
-
-        for (let i = 0; i < this.numSlots; ++i) {
-
-            let slot = slots[i];
-
-            if (!slot.isCardMatch(card))
-                continue ;
-
-            slot.unsetCard();
-
-            return true;
-        }
-
-        return false;
-
-    };
-
-    isValidSelection(): boolean {
-
-        const selectedCards = this.getSelectedCards();
-
-        if (selectedCards.length % 2 === 0)
-            return false;
-
-        for (let i = 0; i < selectedCards.length; ++i) {
-
-            let card = selectedCards[i];
-
-            if (i % 2 === 0) {
-
-                if (!(card instanceof NumberCard))
-                    return false;
-
-            } else {
-
-                if (!(card instanceof OperatorCard))
-                    return false;
-
-            }
-        }
-
-        return true;
-
+      return true;
     }
 
-    getSelectedCards(): CardBase[] {
+    return false;
+  }
 
-        const slots = this.slots.getAll() as CardSlot[];
-        const selectedCards: CardBase[] = [];
+  unsetCardFromSlot(card: CardBase): boolean {
+    const slots = this.slots.getAll() as CardSlot[];
 
-        for (let i = 0; i < this.numSlots; ++i) {
+    for (let i = 0; i < this.numSlots; ++i) {
+      const slot = slots[i];
 
-            let slot = slots[i];
+      if (!slot.isCardMatch(card)) continue;
 
-            if (!slot.isCardSet())
-                continue ;
+      slot.unsetCard();
 
-            let card = slot.getCard() as CardBase;
-
-            selectedCards.push(card);
-
-        }
-
-        return selectedCards;
+      return true;
     }
 
-    align(): void {
+    return false;
+  }
 
-        const slots = this.slots.getAll() as CardSlot[];
+  isValidSelection(): boolean {
+    const selectedCards = this.getSelectedCards();
 
-        Actions.GridAlign(
-            slots, 
-            this.selectedCardAlignConfig.gridOptions
-        );
+    if (selectedCards.length % 2 === 0) return false;
 
-        for (let slot of slots) {
+    for (let i = 0; i < selectedCards.length; ++i) {
+      const card = selectedCards[i];
 
-            slot.setCardPosition();
-
-        }
+      if (i % 2 === 0) {
+        if (!(card instanceof NumberCard)) return false;
+      } else {
+        if (!(card instanceof OperatorCard)) return false;
+      }
     }
+
+    return true;
+  }
+
+  getSelectedCards(): CardBase[] {
+    const slots = this.slots.getAll() as CardSlot[];
+    const selectedCards: CardBase[] = [];
+
+    for (let i = 0; i < this.numSlots; ++i) {
+      const slot = slots[i];
+
+      if (!slot.isCardSet()) continue;
+
+      const card = slot.getCard() as CardBase;
+
+      selectedCards.push(card);
+    }
+
+    return selectedCards;
+  }
+
+  align(): void {
+    const slots = this.slots.getAll() as CardSlot[];
+
+    Actions.GridAlign(slots, this.selectedCardAlignConfig.gridOptions);
+
+    for (const slot of slots) {
+      slot.setCardPosition();
+    }
+  }
 }
