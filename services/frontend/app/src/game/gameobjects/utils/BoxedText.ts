95bdf44156220307a03d10a204920bb27ee0f9de
Takato Mitsuya <142211753+takato7@users.noreply.github.com>
2026-07-08 12:41:00 +0200

    Format code with Prettier

--- services/frontend/app/src/game/gameobjects/utils/BoxedText.ts
+++ services/frontend/app/src/game/gameobjects/utils/BoxedText.ts
@@ -2,43 +2,33 @@ import Phaser, { Scene } from "phaser";
 import CardContent, { type CardContentConfig } from "../cards/CardContent";
 import CardStyle, { type CardStyleConfig } from "../cards/CardStyle";
 
-
 export default class BoxedText extends Phaser.GameObjects.Container {
+  readonly content!: CardContent;
+  readonly style!: CardStyle;
 
-    readonly content!: CardContent;
-    readonly style!: CardStyle;
+  constructor(
+    scene: Scene,
+    text: string,
+    contentConfig: CardContentConfig,
+    styleConfig: CardStyleConfig,
+    posX: number = 0,
+    posY: number = 0,
+  ) {
+    super(scene);
+    scene.add.existing(this);
 
-    constructor(
-        scene: Scene, 
-        text: string, 
-        contentConfig: CardContentConfig, 
-        styleConfig: CardStyleConfig,
-        posX: number = 0,
-        posY: number = 0,
-    ) {
+    this.content = new CardContent(scene, 0, 0, text, contentConfig);
 
-        super(scene);
-        scene.add.existing(this);
+    const { x, y } = this.content.getCenter();
+    this.content.setOrigin(x / this.content.width, y / this.content.height);
 
-        this.content = new CardContent(
-            scene, 0, 0, text, contentConfig
-        );
+    this.style = new CardStyle(scene, 0, 0, styleConfig);
 
-        const {x, y} = this.content.getCenter();
-        this.content.setOrigin(
-            x / this.content.width, y / this.content.height
-        );
+    this.add([this.style, this.content]);
 
-        this.style = new CardStyle(
-            scene, 0, 0, styleConfig
-        )
+    this.setSize(this.style.width, this.style.height);
 
-        this.add([ this.style, this.content ]);
-
-        this.setSize(this.style.width, this.style.height);
-
-        this.setX(posX);
-        this.setY(posY);
-
-    }
+    this.setX(posX);
+    this.setY(posY);
+  }
 }
