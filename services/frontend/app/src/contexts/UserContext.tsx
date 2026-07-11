d9df5bfb5b6217f78eda9e1e9da3fd1a01316734
dlippelt <dlippelt@student.codam.nl>
2026-07-11 12:04:30 +0200

    User Context - REMOVE: remove accidental Key component import

--- services/frontend/app/src/contexts/UserContext.tsx
+++ services/frontend/app/src/contexts/UserContext.tsx
@@ -2,7 +2,6 @@ import { createContext, useContext, /* useEffect, */ useState } from "react";
 import type { ReactNode } from "react";
 import guestAvatar from "../assets/guest_avatar_test.jpg";
 import testAvatar from "../assets/mesca_avatar_test.png";
-import { Key } from "lucide-react";
 
 // TODO: Add Separate ChatHistoryContext and FriendsContext
 
