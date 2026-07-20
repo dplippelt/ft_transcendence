d729871330b820cbd2065a21c9d293e3f5bafca8
bewong <bewong@student.codam.nl>
2026-07-20 19:24:38 +0200

    removed repeat dependency

--- services/backend/app/models/friendship.py
+++ services/backend/app/models/friendship.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from sqlalchemy import DateTime, ForeignKey, Integer, UniqueConstraint, CheckConstraint, func
-from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.database import Base
