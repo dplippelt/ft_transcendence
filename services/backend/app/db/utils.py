8d0872142a73ab31a3cd862ca6605edce5be1ee7
bewong <bewong@student.codam.nl>
2026-07-30 20:11:08 +0200

    format

--- services/backend/app/db/utils.py
+++ services/backend/app/db/utils.py
@@ -4,9 +4,8 @@ from sqlalchemy.orm import Session
 from app.core.exceptions import ErrorCode, bad_request
 
 
-def commit_or_bad_request(db: Session, detail: str, code: ErrorCode) -> None:
-    # Used for writes that may hit DB uniqueness constraints.
-   try:
+def commit_or_bad_request(db: Session, detail: str, code: ErrorCode,) -> None:
+    try:
         db.commit()
     except IntegrityError:
         db.rollback()
