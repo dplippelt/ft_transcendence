d8e5f902f609ddc81403c29c8cbcf75682027f13
Takato Mitsuya <142211753+takato7@users.noreply.github.com>
2026-08-18 15:43:03 +0200

    Moved the logic that moves a card up and down when it is hovered with a mouse

--- services/frontend/app/src/game/gameobjects/cards/CardHand.ts
+++ services/frontend/app/src/game/gameobjects/cards/CardHand.ts
@@ -1,22 +1,16 @@
-import Phaser, { Scene, GameObjects, Geom } from "phaser";
-import CardBase, { CardEvents } from "./CardBase";
+import { Scene, GameObjects } from "phaser";
+import CardBase from "./CardBase";
 
 export default class CardHand {
   private readonly cards: GameObjects.Container;
   private numCards: number;
-//   private focusDiffX: number;
-//   private focusDiffY: number;
 
   constructor(scene: Scene) {
     this.cards = scene.add.container(0, 0);
     this.numCards = 0;
-    // this.focusDiffX = 0;
-    // this.focusDiffY = 0;
   }
 
   addCard(card: CardBase) {
-    // card.on(CardEvents.FOCUSON, this.focusOn, this);
-    // card.on(CardEvents.FOCUSOFF, this.focusOff, this);
     this.cards.add(card);
     this.numCards++;
   }
@@ -33,29 +27,4 @@ export default class CardHand {
   isUnderHandLimit(limit: number) {
     return this.numCards < limit;
   }
-
-//   setFocusDiff(x: number, y: number) {
-//     this.focusDiffX = x;
-//     this.focusDiffY = y;
-//   }
-
-//   focusOn(card: CardBase) {
-//     if (card.getIsSelected()) return;
-
-//     card.setIsFocused(true);
-
-//     if (card.input?.hitArea instanceof Geom.Rectangle) {
-//       card.input.hitArea.right += this.focusDiffX;
-//       card.input.hitArea.bottom += this.focusDiffY;
-//     }
-//   }
-
-//   focusOff(card: CardBase) {
-//     card.setIsFocused(false);
-
-//     if (card.input?.hitArea instanceof Geom.Rectangle) {
-//       card.input.hitArea.right -= this.focusDiffX;
-//       card.input.hitArea.bottom -= this.focusDiffY;
-//     }
-//   }
 }
