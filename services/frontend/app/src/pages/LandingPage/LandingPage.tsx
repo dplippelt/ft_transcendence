9adc6714532bea33ebfb1b3c443aa6a76d192f65
dlippelt <dlippelt@student.codam.nl>
2026-09-19 18:23:11 +0200

    Operator Selection - ADD: add popup pre game start asking for what operators to include in the game - ADD: add url query parameters 'ops' containing operators as a bitmask - ADD: GameManagerScene retrieves the ops bitmask from the url query and parses it into this._operators as an Operator[] - REFACTOR: added buildRoute() utility function that builds a URL from a set of query parameters, in anticipation of multiple query parameters for single-player vs. multiplayer game URLs - REFACTOR: extracted key-value route params from RouteParam into separate RouteParamKey/RouteParamValue enums to support the buildRoute() implementation - NOTE: for invalid ops URL query values, falls back to a default bitmask of '00011' (plus and minus operators only)

--- services/frontend/app/src/pages/LandingPage/LandingPage.tsx
+++ services/frontend/app/src/pages/LandingPage/LandingPage.tsx
@@ -5,7 +5,15 @@ import { MenuButtons } from "../../components/ButtonContainers";
 import Background from "../../components/Background";
 import Page from "../../components/Page";
 import { MenuButton } from "../../components/Buttons";
-import { RouteParam, RoutePath } from "../../utils/utils";
+import { buildRoute, PopupType, RouteParamKey, RouteParamValue, RoutePath } from "../../utils/utils";
+import { useState } from "react";
+import Popup from "../../components/Popup";
+import OperatorSelection from "../../components/OperatorSelection";
+
+interface IButtons
+{
+	setPopupType: React.Dispatch<React.SetStateAction<PopupType>>;
+}
 
 function GameDescription()
 {
@@ -14,14 +22,19 @@ function GameDescription()
 	)
 }
 
-function Buttons()
+function Buttons( { setPopupType } : IButtons )
 {
 	const navigate = useNavigate();
 
+	function handleNewGame()
+	{
+		setPopupType(PopupType.operatorSelection);
+	}
+
 	return (
 		<MenuButtons>
-			<MenuButton label="Start game" onClick={ () => navigate(RoutePath.game) } />
-			<MenuButton label="Login" onClick={ () => navigate(RoutePath.auth + RouteParam.login) } />
+			<MenuButton label="Start game" onClick={handleNewGame} />
+			<MenuButton label="Login" onClick={ () => navigate(buildRoute(RoutePath.auth, { [RouteParamKey.mode]: RouteParamValue.login })) } />
 			<MenuButton label="How to play" onClick={ () => {} } />
 			<MenuButton label="Game dev" onClick={ () => navigate(RoutePath.gameDev) } />
 		</MenuButtons>
@@ -30,13 +43,16 @@ function Buttons()
 
 export default function LandingPage()
 {
+	const [ popupType, setPopupType ] = useState<PopupType>(PopupType.none);
+
 	return (
 		<>
-			<Background/>
+			<Background />
 			<Page>
-				<AppTitle/>
-				<GameDescription/>
-				<Buttons/>
+				<AppTitle />
+				<GameDescription />
+				<Buttons setPopupType={setPopupType} />
+				{ popupType === PopupType.operatorSelection && <Popup> <OperatorSelection setPopupType={setPopupType} /> </Popup> }
 			</Page>
 		</>
 
